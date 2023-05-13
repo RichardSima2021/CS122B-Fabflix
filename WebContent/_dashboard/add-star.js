6cb04fbb84881956dfb55a9bc414974b35c8201d
Richard Sima <richard.sima2021@gmail.com>
2023-05-12 17:50:05 -0700

    starting to write stored procedures for SQL

--- WebContent/_dashboard/add-star.js
+++ WebContent/_dashboard/add-star.js
@@ -1,7 +1,17 @@
 let add_star_form = $("#new_star_form");
 
 function handleAddStarResult(resultData){
+    // console.log("Hello");
+    if(resultData["status"] === "success"){
+        $("#add_star_result_message").text("Successfully added new star, ID: " + resultData["newID"]);
+    }
+    else{
+        $("#add_star_result_message").text("Add star failed: " + resultData["errorMessage"]);
+    }
+}
 
+function handleError(){
+    console.log("something went wrong");
 }
 
 function submitAddStarForm(formSubmitEvent){
@@ -10,9 +20,11 @@ function submitAddStarForm(formSubmitEvent){
     $.ajax(
         "../api/add_star",
         {
+            contentType:"appliation/json",
             dataType:"json",
             method:"GET",
             data:add_star_form.serialize(),
+            error: handleError,
             success:(resultData) => handleAddStarResult(resultData)
         }
     );
--- src/AddStarServlet.java
+++ src/AddStarServlet.java
@@ -4,16 +4,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jdk.jshell.spi.ExecutionControlProvider;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.io.IOException;
 @WebServlet(name = "AddStarServlet", urlPatterns = "/api/add_star")
 public class AddStarServlet extends HttpServlet{
@@ -21,8 +19,7 @@ public class AddStarServlet extends HttpServlet{
 
     public void init(ServletConfig config){
         try{
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jcbd/moviedb");
-        }
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");        }
         catch(NamingException e){
             e.printStackTrace();
         }
@@ -39,6 +36,49 @@ public class AddStarServlet extends HttpServlet{
             year = Integer.parseInt(birthYear);
         }
 
-        System.out.println("Add " + name + " Birthyear: " + year + "|");
+        PrintWriter out = response.getWriter();
+
+//        System.out.println("Add " + name + " Birthyear: " + year + "|");
+
+        try(Connection conn = dataSource.getConnection()){
+            CallableStatement getMaxStarID = conn.prepareCall("CALL getMaxStarID()");
+            ResultSet rs = getMaxStarID.executeQuery();
+            rs.next();
+//            System.out.println(rs.getString("MAX(id)"));
+            int id = Integer.parseInt(rs.getString("MAX(id)").substring(2)) +1;
+            String newID = "nm" + id;
+            String newStarQuery = "INSERT INTO stars(id, name, birthYear) VALUES(?,?,?)";
+            PreparedStatement newStarStatement = conn.prepareStatement(newStarQuery);
+            newStarStatement.setString(1,newID);
+            newStarStatement.setString(2,name);
+            if(year < 0){
+                newStarStatement.setString(3,null);
+            }
+            else{
+                newStarStatement.setInt(3,year);
+            }
+            newStarStatement.executeUpdate();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("status", "success");
+            jsonObject.addProperty("newID", newID);
+
+            rs.close();
+            newStarStatement.close();
+            out.write(jsonObject.toString());
+            response.setStatus(200);
+        }
+        catch(Exception e){
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("status", "failed");
+            jsonObject.addProperty("errorMessage",e.getMessage());
+            System.out.println(jsonObject);
+            out.write(jsonObject.toString());
+            response.setStatus(500);
+        }
+        finally{
+            out.close();
+        }
+
+
     }
 }
